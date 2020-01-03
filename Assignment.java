52134540a461a1489aa1b54bd04dfbe59ce2b373
Oreste Turchetti <theturchetti@berkeley.edu>
2020-01-02 22:21:38 -0600

    More comments.

--- Assignment.java
+++ Assignment.java
@@ -30,10 +30,20 @@ public class Assignment implements Comparable<Assignment>, java.io.Serializable{
 
     }
 
+    /**
+     * Compares assignments to see if they are equal. Assignments are deemed equal if they
+     * have the same name anc category.
+     * @param o Assignment to which this is being compared.
+     * @return true if equal, false if not.
+     */
     public boolean equals( Assignment o) {
         return o.getName().equals(this.getName()) && o.getCategory().equals(this.getCategory());
     }
 
+    /**
+     * Updates percent completion of assignment.
+     * @param percent the new percent completion.
+     */
     public void update(int percent) {
         _completion = percent;
     }
@@ -70,21 +80,33 @@ public class Assignment implements Comparable<Assignment>, java.io.Serializable{
     }
 
     /**
-     * Gets due date.
+     * Gets due date. Accessor method.
      * @return Date at which assignment is due.
      */
     LocalDate getDueDate() {
         return _due;
     }
 
+    /**
+     * Gets name. Accessor method.
+     * @return Name of assignment.
+     */
     String getName() {
         return _name;
     }
 
+    /**
+     * Gets category. Accessor method.
+     * @return Name of category.
+     */
     String getCategory() {
         return _category;
     }
 
+    /**
+     * Gets completion percentage. Accessor method.
+     * @return Int which represents percent of assignment completed.
+     */
     int getCompletion() {
         return _completion;
     }
@@ -92,17 +114,23 @@ public class Assignment implements Comparable<Assignment>, java.io.Serializable{
     /** Due Date of this assignment. */
     private LocalDate _due;
 
+    /** Name of this assignment. */
     private String _name;
 
+    /** Category of this assignment. i.e. name of the class to which the assignment belongs. */
     private String _category;
 
+    /** Date on which the assignment was assigned. */
+    //Currently unused, but planned as a way to sort Q.
     private LocalDate _assigned;
 
+    /** Percent of Assignment completed. */
     private Integer _completion = 0;
 
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
+    /** ANSI escape codes to color print output. */
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_YELLOW = "\u001B[33m";
 
 }
