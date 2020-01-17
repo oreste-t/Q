9ceb571566fb13fcd84a3a6e595500776061c379
Oreste Turchetti <theturchetti@berkeley.edu>
2020-01-16 22:51:48 -0800

    Removed unneccessary test.

--- AssignmentTest.java
+++ AssignmentTest.java
@@ -9,24 +9,8 @@ public class AssignmentTest {
     @Test
     public void basicAssignment() {
         Assignment alpha = new Assignment("HW10", "CS61a",
-                -1, -1, -1);
+                1, 1, 2020);
         assertEquals("Incorrect Name", "HW10", alpha.getName());
-    }
-
-    @Test
-    public void basicQDueDate() {
-        Assignment bravo = new Assignment("HW10", "CS61a",
-                8, 1, 2019);
-        Assignment charlie = new Assignment("WW 1.1", "Math53", 8, 10, 2019);
-
-        Q delta = new Q();
-        delta.push(bravo);
-        delta.push(charlie);
-        assertEquals("Incorrect Order", bravo.getName(), delta.peek().getName());
-
-        Assignment echo = new Assignment("Notes 17.2", "CS170", 7, 20, 2019);
-        delta.push(echo);
-        assertEquals("Incorrect Order", echo.getName(), delta.peek().getName());
     }
 }
 
