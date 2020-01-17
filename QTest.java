8fd2a1c9705d5148a33da0c85eea1ff91d408d54
Oreste Turchetti <theturchetti@berkeley.edu>
2020-01-16 22:44:13 -0800

    Mores tests.

--- QTest.java
+++ QTest.java
@@ -32,12 +32,77 @@ public class QTest {
         assertEquals("Incorrect ordering.", bravo, qu.get(4));
 
         qu.sort(2);
-
         assertEquals("Incorrect ordering.", "CS10", qu.get(0).getCategory());
         assertEquals("Incorrect ordering.", "CS10", qu.get(1).getCategory());
         assertEquals("Incorrect ordering.", "CS188", qu.get(2).getCategory());
         assertEquals("Incorrect ordering.", "CS188", qu.get(3).getCategory());
         assertEquals("Incorrect ordering.", "ESPM10", qu.get(4).getCategory());
+    }
+
+    @Test
+    public void multipleReSort() {
+
+        Assignment alpha = new Assignment("HW1", "CS10",
+                10, 10, 2020);
+        alpha.modifyDateAssigned(1, 5, 2020);
+        alpha.update(33);
+
+        Assignment bravo = new Assignment("HW2", "CS10",
+                10, 15, 2020);
+        bravo.modifyDateAssigned(1, 1, 2020);
+        bravo.update(99);
+
+        Assignment charlie = new Assignment("notes", "CS188",
+                10, 11, 2020);
+        charlie.modifyDateAssigned(3, 28, 2020);
+        charlie.update(100);
+
+        Assignment delta = new Assignment("HW1", "CS188",
+                10, 13, 2020);
+        delta.modifyDateAssigned(1, 2, 2020);
+        delta.update(10);
+
+        Assignment echo = new Assignment("syllabus", "ESPM10",
+                10, 12, 2020);
+        echo.modifyDateAssigned(1, 6, 2020);
+        echo.update(27);
+
+        Q qu = new Q();
+        qu.push(alpha);
+        qu.push(bravo);
+        qu.push(charlie);
+        qu.push(delta);
+        qu.push(echo);
+
+        qu.sort(3);
+        assertEquals("Incorrect ordering.", bravo, qu.get(0));
+        assertEquals("Incorrect ordering.", delta, qu.get(1));
+        assertEquals("Incorrect ordering.", alpha, qu.get(2));
+        assertEquals("Incorrect ordering.", echo, qu.get(3));
+        assertEquals("Incorrect ordering.", charlie, qu.get(4));
+        assertEquals("Incorrect length.", 5, qu.length());
+
+        qu.sort(2);
+        assertEquals("Incorrect ordering.", "CS10", qu.get(0).getCategory());
+        assertEquals("Incorrect ordering.", "CS10", qu.get(1).getCategory());
+        assertEquals("Incorrect ordering.", "CS188", qu.get(2).getCategory());
+        assertEquals("Incorrect ordering.", "CS188", qu.get(3).getCategory());
+        assertEquals("Incorrect ordering.", "ESPM10", qu.get(4).getCategory());
+
+        qu.sort(1);
+        assertEquals("Incorrect ordering.", delta, qu.get(0));
+        assertEquals("Incorrect ordering.", echo, qu.get(1));
+        assertEquals("Incorrect ordering.", alpha, qu.get(2));
+        assertEquals("Incorrect ordering.", bravo, qu.get(3));
+        assertEquals("Incorrect ordering.", charlie, qu.get(4));
+        assertEquals("Incorrect length.", 5, qu.length());
+
+        qu.sort(0);
+        assertEquals("Incorrect ordering.", alpha, qu.get(0));
+        assertEquals("Incorrect ordering.", charlie, qu.get(1));
+        assertEquals("Incorrect ordering.", echo, qu.get(2));
+        assertEquals("Incorrect ordering.", delta, qu.get(3));
+        assertEquals("Incorrect ordering.", bravo, qu.get(4));
     }
 
 
